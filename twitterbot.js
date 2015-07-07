9412b67e0bd90d216888b75f8632839235492f59
Andrew <andrewda@users.noreply.github.com>
2015-07-07 13:57:17 -0700

    Update twitterbot.js

--- twitterbot.js
+++ twitterbot.js
@@ -3,20 +3,21 @@ var Twitter      = require('twitter');
 var EventEmitter = require('events').EventEmitter;
 var em           = new EventEmitter();
 
-var team1    = 'CT'; // Team that starts on CT
-var team2    = 'T'; // Team that starts on T
-var matchid  = 365123;
-var halftime = false;
-var goodToGo = false;
-var tag      = '#' + team1 + 'vs' + team2;
+var team1     = 'VirtusPro'; // Team that starts on CT
+var team2     = 'fnatic'; // Team that starts on T
+var matchid   = 365263;
+var halftime  = false;
+var goodToGo  = false;
+var lastScore = [];
+var tag       = '#' + team1 + 'vs' + team2;
 var winTeam, t1score, t2score, t1t, t2t, t1st, t2st, winner, scoreText, scoreTextSide;
 
 //Twitter login
 var client = new Twitter({
-    consumer_key:        'Ghj2ZiV8bvgveWtqVGa2wuDuX',
-    consumer_secret:     'su6lpyg3lbeAinfYtOxtNOolc7TLjNlbieA5DHQl978f66qqiL',
-    access_token_key:    '2954514570-OaLTeuV69JMnK0MfoH3iiaRMEd7sSUkiVltVMc1',
-    access_token_secret: 'tqLJ8R41y4IjqGsjkIrn51Cnj2W2Pm3jXqweT5PaHaVs1'
+    consumer_key:        'key',
+    consumer_secret:     'secret',
+    access_token_key:    'key',
+    access_token_secret: 'secret'
 });
 
 scorebot.connect('http://scorebot.hltv.org:10022', matchid, em);
@@ -27,7 +28,11 @@ scorebot.on('roundOver', function(data, scores) {
         t1score       = scores.ct;
         t2score       = scores.t;
         scoreText     = '#' + team1 + ' ' + t1score + ' : ' + t2score + ' #' + team2;
-        scoreTextSide = '#' + team1 + ' (CT) ' + t1score + ' : ' + t2score + ' #' + team2 + ' (T)';
+        scoreTextSide = '#' + team1 + ' (CT) ' + t1score + ' : ' + t2score + ' (T) #' + team2;
+        lastScore     = [
+            Number(t1score),
+            Number(t2score)
+            ];
         
         if (winTeam == 'CT') {
             winner = team1;
@@ -35,18 +40,13 @@ scorebot.on('roundOver', function(data, scores) {
             winner = team2;
         }
         
-        if (Number(t1score) + Number(t2score) == 16) {
-            t1st    = t1score;
-            t2st    = t2score;
-            t1score = t2st;
-            t2score = t1st;
-            
+        if (Number(t1score) + Number(t2score) + 1 == 16) {
             if (winTeam == 'CT') {
-                t1score = Number(Number(t1score) + 1).toString();
-                t2score = Number(Number(t2score) - 1).toString();
-            } else if (winTeam == 'T') {
                 t1score = Number(Number(t1score) - 1).toString();
                 t2score = Number(Number(t2score) + 1).toString();
+            } else if (winTeam == 'T') {
+                t1score = Number(Number(t1score) + 1).toString();
+                t2score = Number(Number(t2score) - 1).toString();
             }
         }
         
@@ -83,7 +83,11 @@ scorebot.on('roundOver', function(data, scores) {
 
 scorebot.on('scoreUpdate', function(t, ct) {
     if (!goodToGo) {
-        goodToGo = true;
+        goodToGo  = true;
+        lastScore = [
+            Number(ct), 
+            Number(t)
+            ];
         console.log("Good-To-Go!", '(CT - ' + ct + ' | ' + t + ' - T)');
     }
 });
