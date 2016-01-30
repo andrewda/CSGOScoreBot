6d520fdab9d35596ad9625ec83be827eb8b21cfe
Andrew <dassonville.andrew@gmail.com>
2016-01-30 07:13:05 +0000

    Make various changes and fixes

--- redditbot.js
+++ redditbot.js
@@ -7,51 +7,98 @@ var lg = new LiveGames({ polling: 5000 });
 var sb = new Scorebot();
 
 var connected = false;
-var tname = '';
-var cname = '';
+
+var tname;
+var cname;
+var bestof;
+var date;
+var time;
 
 reddit.setupOAuth2('id', 'secret');
 
 lg.on('newGame', function(game) {
+    console.log('GAME NOW LIVE: ' + game.matchid);
     if (!connected) {
         console.log(game);
         
+        tname = game.teams[0];
+        cname = game.teams[1];
+        
+        bestof = game.bestof;
+        
+        date = game.date;
+        time = game.time;
+        
         sb.connect(game.matchid, game.listid);
         connected = true;
-        
-        reddit.auth({'username': 'usrnm', 'password': 'passwd'}, function(err, response) {
-            if(err) {
-                console.log('Unable to authenticate user: ' + err);
-            } else {
-                // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
-                // and will be valid for response.expires_in seconds.
-                // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
-                
-                reddit.createLiveThread({
-                    title: game.teams[0] + ' vs. ' + game.teams[1],
-                    description: ''
-                }, function (err, id) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(id);
-                        
-                        sb.on('roundEnd', function(data) {
-                            var message = data.winner + ' wins the round - (' + cname + ') ' + data.score.ct + ' : ' + data.score.t + ' (' + tname + ')'; 
-                            reddit.liveUpdate(id, message, function(err) {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-                        });
-                        
-                        sb.on('scoreboard', function(data) {
-                            tname = data.terroristTeamName;
-                            cname = data.ctTeamName;
-                        });
-                    }
-                });
-            }
-        });
     }
+});
+
+sb.on('connected', function() {
+    reddit.auth({'username': 'usr', 'password': 'pass'}, function(err, response) {
+        if(err) {
+            console.log('Unable to authenticate user: ' + err);
+        } else {
+            reddit.createLiveThread({
+                title: tname + ' vs. ' + cname,
+                description: ''
+            }, function (err, id) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(id);
+                    
+                    reddit.inviteLiveContributor(id, 'admin', ['close', 'edit', 'manage', 'settings', 'update'], function(err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    
+                    /*
+                    reddit.captcha(function(err, captcha) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(captcha);
+                        }
+                    });
+                    
+                    reddit.submit({
+                        title: tname + ' vs. ' + cname + ' | ' + bestof + ' | ' + date + ' | ' + time + ' CEST',
+                        url: 'https://www.reddit.com/live/' + id,
+                        save: false,
+                        inboxReplies: true,
+                        r: 'CSGOScores',
+                    }, function(err, id) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    */
+                    
+                    sb.on('roundEnd', function(data) {
+                        var winner;
+                        
+                        if (data.winner === 0) {
+                            winner = tname;
+                        } else {
+                            winner = cname;
+                        }
+                        
+                        var message = winner + ' wins the round - (' + cname + ') ' + data.score.ct + ' : ' + data.score.t + ' (' + tname + ')'; 
+                        reddit.liveUpdate(id, message, function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    });
+                    
+                    sb.on('scoreboard', function(data) {
+                        tname = data.terroristTeamName;
+                        cname = data.ctTeamName;
+                    });
+                }
+            });
+        }
+    });
 });
